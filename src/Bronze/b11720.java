b949d973a5a3bda42d2fb3127d30103e5842b254
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2024-12-17 15:11:27 +0900

    b11720

---
+++ src/Bronze/b11720.java
@@ -0,0 +1,18 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class b11720 {
+    static public void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+
+        for(int i = 0; i < cNum.length; i++){
+            sum += cNum[i] - '0';
+        }
+        System.out.println(sum);
+    }
+}
