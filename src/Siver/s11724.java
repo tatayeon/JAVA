17ad0232d96d63c90393e238e116b68e49cdada0
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2024-12-26 22:40:44 +0900

    1d1c강의

---
+++ src/Siver/s11724.java
@@ -0,0 +1,56 @@
+package Siver;
+
+// DFS 중요한 문제!!!
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class s11724 {
+
+    static boolean visioted[];
+    static ArrayList<Integer>[] A;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        visioted = new boolean[n+1];
+        A = new ArrayList[n+1];
+        for(int i=1; i<=n; i++){
+            A[i] = new ArrayList<Integer>();
+        }
+
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e); // 둘다 연결이 되어있기 때문에 이렇게 양쪽 방향을 다 더해야한다.
+            A[e].add(s);
+        }
+        int count = 0;
+        for(int i = 1; i < n+1; i++){
+            if(!visioted[i]){
+                count++;
+                DFS(i);
+            }
+        }
+        System.out.println(count);
+
+    }
+
+    private static void DFS(int v) {
+        if(visioted[v]){
+            return;
+        }
+        visioted[v] = true;
+        for(int i : A[v]){
+            if(!visioted[i]){
+                DFS(i);
+            }
+        }
+    }
+
+}
---
+++ src/Siver/s1427.java
@@ -0,0 +1,37 @@
+package Siver;
+
+import java.util.Scanner;
+
+// 선택 정렬
+public class s1427 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int A[] = new int[str.length()];
+
+        for(int i = 0; i < str.length(); i++){
+            A[i] = Integer.parseInt(str.substring(i, i+1));
+        }
+
+        for(int i = 0 ; i < str.length(); i++){
+            int Max = i;
+            for(int j = i+1 ; j < str.length(); j++){
+                if(A[j] > A[Max]){
+                    Max = j;
+                }
+            }
+            if(A[Max] > A[i]){
+                int temp = A[i];
+                A[i] = A[Max];
+                A[Max] = temp;
+            }
+        }
+
+        for(int i = 0 ; i < str.length(); i++){
+            System.out.print(A[i]);
+        }
+
+
+    }
+}
