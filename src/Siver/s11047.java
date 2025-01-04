32745c788000a58bb00d0f1b0fc22d162ab91699
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2025-01-04 22:31:50 +0900

    1d1c-강의

---
+++ src/Siver/s11047.java
@@ -0,0 +1,28 @@
+package Siver;
+
+import java.util.Scanner;
+//그리드 알고리즘 그리드 알고리즘은 항상 최선의 값을 도출해 내는 알고리즘이다.
+public class s11047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] arr = new int[N];
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = arr.length - 1; i >= 0; i--){
+            if(arr[i] <= K){
+                count += K / arr[i];
+                K = K % arr[i];
+            }
+        }
+        System.out.println(count);
+
+
+
+    }
+}
