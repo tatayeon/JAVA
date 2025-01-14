1ad42b1603a647522a4d1a8bdac05c72b0cb2851
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2025-01-14 16:32:18 +0900

    1d1c

---
+++ src/Bronze/b2231.java
@@ -0,0 +1,32 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class b2231 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int result = 0;
+
+        for(int i = 0; i < N; i++) {
+            int number = i;
+            int sum = 0;	// 각 자릿수 합 변수
+
+            while(number != 0) {
+                sum += number % 10;	// 각 자릿수 더하기
+                number /= 10;
+            }
+
+            // i 값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우)
+            if(sum + i == N) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+
+
+
+    }
+}
