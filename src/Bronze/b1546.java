20bdc5867f8778fbc150409a1e208d5f28c3d9b1
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2025-03-20 15:19:34 +0900

    구간 합

--- src/Bronze/b1546.java
+++ src/Bronze/b1546.java
@@ -1,22 +1,28 @@
 package Bronze;
 
 import java.util.Scanner;
+// 평균 구하기
 
 public class b1546 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        int[] arr = new int[N];
+        long Max = 0;
         long sum = 0;
-        long max = 0;
 
-        for(int i = 0; i <N; i++){
-            int temp = sc.nextInt();
-            sum += temp;
-            if(temp > max){
-                max = temp;
-            }
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.println(sum*100.0/max/N);
+
+        for (int i = 0; i < N; i++) {
+            if(arr[i] > Max){
+                Max = arr[i];
+            }
+            sum += arr[i];
+        }
+
+        System.out.println(sum * 100.0 / Max / N);
 
     }
 }
