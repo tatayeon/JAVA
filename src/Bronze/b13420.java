37bf2249ff056b70984f4c70b3732a708599ae88
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2025-04-04 02:41:07 +0900

    1d1c

---
+++ src/Bronze/b13420.java
@@ -0,0 +1,20 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class b13420 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        String[] input = br.readLine().split(" ");
+
+    }
+
+}
