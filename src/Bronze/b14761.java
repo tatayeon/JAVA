45a83b340e434174cd61c511505108170324d3d3
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2025-04-14 23:38:13 +0900

    1d1c

---
+++ src/Bronze/b14761.java
@@ -0,0 +1,21 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class b14761 {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            String[] inputStr = sc.next().split("");
+            count += inputStr.length;
+        }
+
+        System.out.print(count);
+
+    }
+}
