22237bd9d267451268ade86c876f8be27465b77f
limtaeyeon <dlaxodus1212@naver.com>
2025-07-17 22:31:38 +0900

    1d1c

---
+++ src/Silver/s4375.java
@@ -0,0 +1,26 @@
+package Silver;
+
+import java.util.Scanner;
+
+public class s4375 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int temp = 1;
+        int count=0;
+        int N = sc.nextInt();
+
+        while(true){
+            count++;
+            temp = count*N;
+
+//            for(int i=0; i<count; i++){
+//                if(temp % (count*i) == 1){
+//                    count++;
+//                }else {
+//                    String result = String.valueOf(temp);
+//                    System.out.print(result.length());
+//                }
+//            }
+        }
+    }
+}
